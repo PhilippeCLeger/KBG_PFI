ad3e1bb66c41ac189fa78326b1c038a83acebc9b
marianstici <78760234+marianstici@users.noreply.github.com>
2022-11-18 11:21:20 -0500

    Register

--- wwwroot/ImagesManager2022/js/index.js
+++ wwwroot/ImagesManager2022/js/index.js
@@ -2,4 +2,7 @@ const baseURL = "http://localhost:5000"
 const accountsAPI = new AccountsAPI(baseURL);
 const loginDialog = new LoginDialog(
     $("#loginDlg"), $("#email_input"), $("#password_input"), accountsAPI);
-loginDialog.show();
+//loginDialog.show();
+const registerDialog = new RegisterDialog(
+    $("#registerDlg"),$("#name_input"), $("#email_input"), $("#password_input"), accountsAPI);
+
---
+++ wwwroot/ImagesManager2022/js/register.js
@@ -0,0 +1,71 @@
+class RegisterDialog{
+    constructor (dlg, nameInput,emailInput, passwordInput, AccountsAPI){
+        this.dlg = dlg;
+        this.nameInput = nameInput;
+        this.emailInput = emailInput;
+        this.passwordInput = passwordInput;
+        this.API = AccountsAPI;
+        this.__initialize_dialog();
+    }
+
+    __initialize_dialog(){
+        this.dlg.dialog({
+            title: "Se Register",
+            autoOpen: false,
+            modal: true,
+            show: {effect: 'fade', speed: 400},
+            hide: {effect: 'fade', speed: 400},
+            width: 500, minWidth: 500, maxWidth: 500,
+            height: 230, minHeight: 230, maxHeight: 230,
+            position: {},
+            buttons: [
+                {
+                    text: "Envoyer",
+                    click: (e) => {
+                        e.preventDefault();
+                        this.register();
+                    }
+                },
+                {
+                    text: "Annuler",
+                    click: () => {
+                        this.hide();
+                    }
+                }
+            ]
+        });
+    }
+
+
+    register(){
+        let name = this.nameInput.val();
+        let email = this.emailInput.val();
+        let password = this.passwordInput.val();
+        const successLoginCallback = (data) => {
+            console.log("success:");
+            console.log(data);
+            this.hide();
+        };
+
+        const errorLoginCallback = (error) => {
+            console.log("error");
+            console.log(error);
+        };
+
+        this.API.register(name, email, password, successLoginCallback, errorLoginCallback);
+    }
+
+    show(){
+        this.dlg.dialog('open');
+    }
+
+    hide(){
+        this.dlg.dialog('close');
+    }
+
+    empty(){
+        this.nameInput.val("");
+        this.emailInput.val("");
+        this.passwordInput.val("");
+    }
+}
