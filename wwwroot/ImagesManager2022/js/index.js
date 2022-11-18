e1d3d91e8262dbe0b81ee82074e13aa7810f7576
PhilippeCLeger <philippe-cosmo.leger@outlook.com>
2022-11-18 11:42:35 -0500

    Ajout des boutons pour login, register et edit profile

--- wwwroot/ImagesManager2022/js/index.js
+++ wwwroot/ImagesManager2022/js/index.js
@@ -3,6 +3,21 @@ const accountsAPI = new AccountsAPI(baseURL);
 const userData = new UserData();
 const loginDialog = new LoginDialog(
     $("#loginDlg"), $("#email_input"), $("#password_input"), $("#remember_input"), accountsAPI, userData);
-loginDialog.show();
 const registerDialog = new RegisterDialog(
     $("#registerDlg"),$("#name_input"), $("#email_input"), $("#password_input"), accountsAPI);
+
+const btnLogin = $("#btnLogin");
+const btnRegister = $("#btnSignup");
+const btnProfile = $("#btnProfile");
+
+
+btnLogin.click((e) => {
+    e.preventDefault();
+    loginDialog.show();
+})
+
+btnRegister.click((e) => {
+    e.preventDefault();
+    registerDialog.show();
+});
+
