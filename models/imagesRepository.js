49d6be761878e2ae20a1d450cd8887ffe1ebf5c2
PhilippeCLeger <philippe-cosmo.leger@outlook.com>
2022-12-14 21:01:02 -0500

    Ajout du filtre par mots clés, côté serveur

--- models/imagesRepository.js
+++ models/imagesRepository.js
@@ -2,6 +2,7 @@
 const ImageFilesRepository = require('./imageFilesRepository.js');
 const UsersRepository = require('./usersRepository');
 const ImageModel = require('./image.js');
+const CollectionFilter = require("./collectionFilter.js");
 const utilities = require("../utilities");
 const HttpContext = require('../httpContext').get();
 
@@ -63,4 +64,33 @@ module.exports =
             }
             return false;
         }
+
+        getAll(params = null) {
+            let imagesList = this.objects();
+            if (this.bindExtraDataMethod != null) {
+                imagesList = this.bindExtraData(imagesList);
+            }
+            if (params) {
+                imagesList = this.filterImagesByKeyWords(params, imagesList);
+                let collectionFilter = new CollectionFilter(
+                    imagesList,
+                    params,
+                    this.model
+                );
+                return collectionFilter.get();
+            }
+            return imagesList;
+        }
+        
+        filterImagesByKeyWords(params, imagesList){
+            if(!("keywords" in params)) return imagesList;
+            let keywords = params.keywords.toLowerCase().split(" ");
+            delete params.keywords;
+            return imagesList.filter((image) => {
+                return keywords.filter((kw) => {
+                    return !image.Title.toLowerCase().includes(kw) && !image.Description.toLowerCase().includes(kw);
+                }).length == 0;
+            });
+        }
+
     }
